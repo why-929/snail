486a61ac772407002732c955e1904d7f4a7b3ae3
e <15662023090@163.com>
2020-06-06 17:41:10 +0800

    首页banner的js和其他页面布局优化

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,18 @@
 /* top-nav部分 */
 //鼠标放下出现下拉菜单
-// !function($) {
-//     $('.showNav').on('mouseenter', function() {
-//         console.log(111);
-//         const $hiddenNav = $('.hiddenNav');
-//             $hiddenNav.stop(true).animate({
-//                 top: 0
-//             })
-//     });
-// }(jQuery);
+!function($) {
+    const $showli=$('.showNav .dropBox');
+    const $hideNav = $('.hideNav');
+    $showli.hover(function() {
+        $hideNav.stop(true).animate({
+            top: 0
+        })
+    },function(){
+        $hideNav.stop(true).animate({
+            top: -190
+        })
+    });
+}(jQuery);
 
 //轮播图 - 二级菜单
 !function($){
@@ -36,8 +40,70 @@
 }(jQuery)
 
 //轮播图
- 
+!function(){
+    const $banner = $('.banner');//轮播图盒子
+    const $bannerUl = $('.banner ul');
+    const $Imgli = $('.banner ul li');//五张图片
+    const $circleBtn = $('.banner ol li');//五个圆圈按钮
+    const $leftBtn = $('.leftBtn');
+    const $rightBtn = $('.rightBtn');
+    let $index = 0;
+    let $timer = null;
+    //1.改变布局
+    let $cloneImg = $Imgli.first().clone(true, true); //克隆第一张图片
+    let $Imgwidth = $Imgli.eq(0).width();//图片的宽度
+    $bannerUl.append($cloneImg).css({//追加并设置图片盒子的宽度
+        width: $bannerUl.children().length * $Imgwidth
+    });
 
+    //封装运动过程
+    function bannerSwitch(){
+        $index ++;
+        //当点击到索引为最后clone的那张时，将图片拉回
+        if($index === $circleBtn.length + 1){
+            $bannerUl.css({
+                left:0
+            });
+            $index = 1;//第二张图片
+        }
+        //当点击到索引为第一张之前时,拉到最后一张
+        if($index === -1){
+            $bannerUl.css({
+                left:-$circleBtn.length * $Imgwidth
+            });
+            $index = $circleBtn.length - 1;
+        }
+        //当索引为最后一个圆圈按钮时,给第一个小圆圈按钮添加类名
+        if($index === $circleBtn.length){
+            $circleBtn.eq(0).addClass('olActive').siblings('ol li').removeClass('olActive');
+        }else{
+            $circleBtn.eq($index).addClass('olActive').siblings('ol li').removeClass('olActive')
+        }
+        //给图片盒子加left值让图片运动
+        $bannerUl.stop(true).animate({
+            left: -$Imgwidth * $index
+        }); 
+    }
+
+    //2.点击圆圈按钮，图片位置发生变化
+    $circleBtn.on('click',function(){
+        $index = $(this).index() - 1;//-1抵消bannerSwitch()中的index++
+        bannerSwitch()
+    })
+
+    //3.箭头添加点击事件
+    $rightBtn.on('click',function(){
+        bannerSwitch();
+    })
+    $leftBtn.on('click',function(){
+        $index -=2;//bannerSwitch()中index+1,本身应该-1,所以-2
+        bannerSwitch();
+    })
+
+    $timer = setInterval(() => {
+        bannerSwitch();
+    }, 3500);
+}(jQuery)
 
 // 回到顶部
 !function($){
